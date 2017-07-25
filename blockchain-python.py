de24d6b5598ffe407a96578a5939ec0543907ebe
Aditya Asgaonkar <aditya.asgaonkar@gmail.com>
2017-07-25 18:29:40 +0530

    Added mine_block()

--- blockchain-python.py
+++ blockchain-python.py
@@ -4,20 +4,35 @@ class Block:
     def __init__(self, data, previous_hash):
         self.data = data
         self.previous_hash = previous_hash
+        self.nonce = 0
+        self.difficulty = 2
         self.hash = self.hash_block()
+        if(self.previous_hash!='0'):
+            self.mine_block()
 
     def hash_block(self):
         sha = hashlib.sha256()
-        sha.update((str(self.data) + str(self.previous_hash)).encode('utf-8'))
+        sha.update((str(self.data) + str(self.previous_hash) + str(self.nonce)).encode('utf-8'))
         return sha.hexdigest()
 
+    def mine_block(self):
+        prefix = '0' * self.difficulty
+        while(not( ( self.hash ).startswith(prefix) )):
+            self.nonce += 1
+            self.hash = self.hash_block()
+        return
+
     def print_block(self):
-        print("Block:\n\tData:\t\t", self.data, "\n\tPrevious Hash:\t", self.previous_hash[:10], "\n\tHash:\t\t", self.hash[:10])
+        print("Block:\n\tData:\t\t", self.data)
+        print("\tPrevious Hash:\t", self.previous_hash[:10])
+        print("\tNonce:\t\t", self.nonce)
+        print("\tDifficulty:\t", self.difficulty)
+        print("\tHash:\t\t", self.hash[:10])
         return
 
     @staticmethod
     def generate_genesis_block():
-        return Block({"Data": "Genesis Block"}, "0")
+        return Block({"Data": "Genesis Block"}, '0')
 
 
 blockchain = [Block.generate_genesis_block()]
