a1a4f2dafb030f67b199615422c5afc9562f0fb3
Aditya Asgaonkar <aditya.asgaonkar@gmail.com>
2017-10-01 20:52:35 +0530

    Added test data

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -16,7 +16,7 @@ PROG="simulate.py"
 
 number_of_peers=(15)
 simulation_time=600
-lambda_sync=('0.3' '0.6' '0.9')
+lambda_sync=('0.47' '0.93' '1.4')
 lambda_gen=('0.2' '0.4' '0.6')
 
 for peer in ${number_of_peers[@]}; do
@@ -30,7 +30,7 @@ done
 
 number_of_peers=(20)
 simulation_time=600
-lambda_sync=('0.4' '0.8' '1.2')
+lambda_sync=('0.84' '1.69' '2.53')
 lambda_gen=('0.2' '0.4' '0.6')
 
 for peer in ${number_of_peers[@]}; do
@@ -44,7 +44,7 @@ done
 
 number_of_peers=(25)
 simulation_time=600
-lambda_sync=('0.5' '1.0' '1.5')
+lambda_sync=('1.33' '2.67' '4.0')
 lambda_gen=('0.2' '0.4' '0.6')
 
 for peer in ${number_of_peers[@]}; do
@@ -55,3 +55,46 @@ for peer in ${number_of_peers[@]}; do
         done
     done
 done
+
+number_of_peers=(15)
+simulation_time=600
+lambda_sync=('0.47' '0.93' '1.4')
+lambda_gen=('0.2' '0.4' '0.6')
+
+for peer in ${number_of_peers[@]}; do
+    for gen in ${lambda_gen[@]}; do
+        for sync in ${lambda_sync[@]}; do
+            echo "python3 $PROG $peer $sync $gen $simulation_time"
+            python3 $PROG $peer $sync $gen $simulation_time
+        done
+    done
+done
+
+number_of_peers=(20)
+simulation_time=600
+lambda_sync=('0.84' '1.69' '2.53')
+lambda_gen=('0.2' '0.4' '0.6')
+
+for peer in ${number_of_peers[@]}; do
+    for gen in ${lambda_gen[@]}; do
+        for sync in ${lambda_sync[@]}; do
+            echo "python3 $PROG $peer $sync $gen $simulation_time"
+            python3 $PROG $peer $sync $gen $simulation_time
+        done
+    done
+done
+
+number_of_peers=(25)
+simulation_time=600
+lambda_sync=('1.33' '2.67' '4.0')
+lambda_gen=('0.2' '0.4' '0.6')
+
+for peer in ${number_of_peers[@]}; do
+    for gen in ${lambda_gen[@]}; do
+        for sync in ${lambda_sync[@]}; do
+            echo "python3 $PROG $peer $sync $gen $simulation_time"
+            python3 $PROG $peer $sync $gen $simulation_time
+        done
+    done
+done
+
