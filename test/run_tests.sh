f58607bd053697e576e56cdd3f75a6b67a42fa21
Aditya Asgaonkar <aditya.asgaonkar@gmail.com>
2017-10-06 12:39:32 +0530

    Added n*log(n) data

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,19 +1,5 @@
 PROG="simulate.py"
 
-number_of_peers=(10)
-simulation_time=600
-lambda_sync=('0.2' '0.4' '0.6')
-lambda_gen=('0.2' '0.4' '0.6')
-
-for peer in ${number_of_peers[@]}; do
-   for gen in ${lambda_gen[@]}; do
-       for sync in ${lambda_sync[@]}; do
-           echo "python3 $PROG $peer $sync $gen $simulation_time"
-           python3 $PROG $peer $sync $gen $simulation_time
-       done
-   done
-done
-
 number_of_peers=(15)
 simulation_time=600
 lambda_sync=('0.35' '0.71' '1.06')
